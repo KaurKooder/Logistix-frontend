b81c883f5704e7556832647248f80ad4fade419d
Helena <hejoos@taltech.ee>
2025-10-02 22:21:25 +0300

    Lisasin funktsiooni, et blogipostituse lehele saab ka minna

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 import Users from './views/Users.vue';
+import BlogPosts from "@/views/BlogPosts.vue";
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/users', name: 'Users', component: Users },
+  { path: '/posts', name: 'BlogPosts', component: BlogPosts}
 ];
 
 const router = createRouter({
