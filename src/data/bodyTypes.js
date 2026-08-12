9768d1a0fc1adad2bf7780271f5c7696cdb61264
Kaur Drenkhan <kaur@senes.ee>
2026-08-12 13:44:39 +0300

    veokide bodytyped.

---
+++ src/data/bodyTypes.js
@@ -0,0 +1,48 @@
+// Grouped as [{ group, options }] so dropdowns can show category headers.
+export const bodyTypes = [
+  {
+    group: 'General / dry freight',
+    options: [
+      'Box',
+      'Box with tail lift',
+      'Curtain',
+      'Tautliner',
+      'Flatbed truck',
+      'Drop side',
+      'Jumbo',
+      'Mega',
+    ],
+  },
+  {
+    group: 'Temperature controlled',
+    options: ['Refrigerated body / Reefer', 'Thermo'],
+  },
+  {
+    group: 'Tipper & bulk',
+    options: ['Tipper', 'Walking floor', 'Walking floor (Bulk material)', 'Skip loader'],
+  },
+  {
+    group: 'Tank & liquid',
+    options: ['Tank trailer'],
+  },
+  {
+    group: 'Container',
+    options: ['Container chassis', 'Skeletal trailer', 'Swap bodies - Chassis', 'Roll-off container'],
+  },
+  {
+    group: 'Specialized',
+    options: [
+      'Car transporter',
+      'Livestock body',
+      'Timber trailer',
+      'Low loader',
+      'Extendable trailer',
+      'Coil trough',
+      'Inloader',
+      'Special truck',
+      'Tow/Recovery body',
+      'Tractor',
+      'Belt trailer',
+    ],
+  },
+]
